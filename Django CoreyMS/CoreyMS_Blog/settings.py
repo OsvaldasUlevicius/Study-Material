292da00b9e1402785094605b67cfdfb5566716a3
Osvaldas Ulevičius <osvaldas.ulevicius@gmail.com>
2020-10-01 13:03:11 +0300

    Update settings.py

--- Django CoreyMS/CoreyMS_Blog/settings.py
+++ Django CoreyMS/CoreyMS_Blog/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yc9cpfyst!b)fccbaw&cpf!n(yiq--xjvy==drkzzb5=)2&rk0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
